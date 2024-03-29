e670b633b98767579d809042b09410251127fabc
Le Nguyen <97854609+lenguyen2022@users.noreply.github.com>
2024-03-29 18:30:39 -0400

    Update Root schema.sql

--- GBADS_TermEnrichment/schema.sql
+++ GBADS_TermEnrichment/schema.sql
@@ -17,29 +17,11 @@ DROP TABLE IF EXISTS `termdb`.`term` ;
 CREATE TABLE IF NOT EXISTS `termdb`.`term` (
   `term_id` BIGINT NOT NULL AUTO_INCREMENT,
   `species` VARCHAR(45) NOT NULL,
-  `superClass`  VARCHAR(45) NOT NULL,
+  `super_class`  VARCHAR(45) NOT NULL,
   `country` VARCHAR(45) NULL,
-  `t_year` VARCHAR(45) NULL,
+  `term_year` VARCHAR(45) NULL,
+  `data_source` VARCHAR(300) NULL,
   `description` VARCHAR(400) NULL,
-  `ontologyVersion` VARCHAR(200) NOT NULL,
+  `ontology_version` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`term_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `termdb`.`enriched_term`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `termdb`.`enriched_term` ;
-
-CREATE TABLE IF NOT EXISTS `termdb`.`enriched_term` (
-  `enriched_term_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `species` VARCHAR(45) NOT NULL,
-  `term_id` BIGINT NOT NULL,
-  PRIMARY KEY (`enriched_term_id`),
-  INDEX `fk_enriched_term_term_idx` (`term_id` ASC) VISIBLE,
-  CONSTRAINT `fk_enriched_term`
-    FOREIGN KEY (`term_id`)
-    REFERENCES `termdb`.`term` (`term_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
